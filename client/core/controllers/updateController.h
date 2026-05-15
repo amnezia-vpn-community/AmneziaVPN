ff92e23483d246965078f8ea5275d6168c4318f9
amneziabot <amneziabot@users.noreply.github.com>
2026-05-15 17:39:31 +0000

    security: harden updater transport trust

--- client/core/controllers/updateController.h
+++ client/core/controllers/updateController.h
@@ -4,6 +4,7 @@
 #include <functional>
 #include <QObject>
 #include <QNetworkReply>
+#include <QUrl>
 
 #include "core/repositories/secureAppSettingsRepository.h"
 
@@ -16,6 +17,9 @@ public:
     QString getRawChangelogText() const;
     QString getReleaseDate() const;
     QString getVersion() const;
+
+    static bool isValidUpdaterBaseUrl(const QString &baseUrl);
+    static bool isValidUpdaterUrl(const QUrl &url);
 
 public slots:
     void checkForUpdates();
---
+++ client/tests/testUpdateControllerSecurity.cpp
@@ -0,0 +1,42 @@
+#include <QTest>
+#include <QUrl>
+
+#include "core/controllers/updateController.h"
+
+class TestUpdateControllerSecurity : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void rejectsPlainHttpUpdaterBaseUrl()
+    {
+        QVERIFY(!UpdateController::isValidUpdaterBaseUrl(QStringLiteral("http://updates.amnezia.org")));
+        QVERIFY(!UpdateController::isValidUpdaterBaseUrl(QStringLiteral("http://updates.amnezia.org:80/releases")));
+    }
+
+    void rejectsCredentialsQueryAndFragmentInBaseUrl()
+    {
+        QVERIFY(!UpdateController::isValidUpdaterBaseUrl(QStringLiteral("https://user:pass@updates.amnezia.org")));
+        QVERIFY(!UpdateController::isValidUpdaterBaseUrl(QStringLiteral("https://updates.amnezia.org/releases?channel=stable")));
+        QVERIFY(!UpdateController::isValidUpdaterBaseUrl(QStringLiteral("https://updates.amnezia.org/releases#latest")));
+    }
+
+    void acceptsHttpsUpdaterBaseUrl()
+    {
+        QVERIFY(UpdateController::isValidUpdaterBaseUrl(QStringLiteral("https://updates.amnezia.org/releases")));
+    }
+
+    void rejectsUnsafeInstallerUrl()
+    {
+        QVERIFY(!UpdateController::isValidUpdaterUrl(QUrl(QStringLiteral("http://updates.amnezia.org/AmneziaVPN.exe"))));
+        QVERIFY(!UpdateController::isValidUpdaterUrl(QUrl(QStringLiteral("https://user@updates.amnezia.org/AmneziaVPN.exe"))));
+    }
+
+    void acceptsHttpsInstallerUrl()
+    {
+        QVERIFY(UpdateController::isValidUpdaterUrl(QUrl(QStringLiteral("https://updates.amnezia.org/AmneziaVPN_1.2.3_x64.exe"))));
+    }
+};
+
+QTEST_MAIN(TestUpdateControllerSecurity)
+#include "testUpdateControllerSecurity.moc"
