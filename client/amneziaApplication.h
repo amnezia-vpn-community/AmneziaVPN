c7ed92ee3ba820fe56c201a1f3b111528b095f6d
amneziabot <amneziabot@users.noreply.github.com>
2026-05-20 11:19:02 +0000

    Redact import and process argument logging

--- client/amneziaApplication.h
+++ client/amneziaApplication.h
@@ -66,6 +66,7 @@ private:
     QCommandLineOption m_optCleanup;
     QCommandLineOption m_optConnect;
     QCommandLineOption m_optImport;
+    QCommandLineOption m_optImportFile;
 
     QSharedPointer<VpnConnection> m_vpnConnection;
     QThread m_vpnConnectionThread;
