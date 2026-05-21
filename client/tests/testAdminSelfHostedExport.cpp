d82dea19c695325988ebf75e055af0fd7c0258f3
amneziabot <amneziabot@users.noreply.github.com>
2026-05-21 16:18:17 +0000

    security: stop logging admin export test secrets

--- client/tests/testAdminSelfHostedExport.cpp
+++ client/tests/testAdminSelfHostedExport.cpp
@@ -3,7 +3,6 @@
 #include <QJsonObject>
 #include <QJsonArray>
 #include <QFile>
-#include <QDebug>
 #include <QUuid>
 #include <QSignalSpy>
 
@@ -19,34 +18,6 @@ class TestAdminSelfHostedExport : public QObject
 private:
     CoreController* m_coreController;
     SecureQSettings* m_settings;
-
-    QJsonObject decodeVpnKey(const QString &vpnKey) {
-        QString key = vpnKey;
-        key.replace("vpn://", "");
-        
-        QByteArray ba = QByteArray::fromBase64(
-            key.toUtf8(), 
-            QByteArray::Base64UrlEncoding | QByteArray::OmitTrailingEquals
-        );
-        
-        qDebug() << "Base64 decoded size:" << ba.size();
-        
-        QJsonDocument testDoc = QJsonDocument::fromJson(ba);
-        if (!testDoc.isNull()) {
-            qDebug() << "Data is not compressed, using as-is";
-            return testDoc.object();
-        }
-        
-        QByteArray baUncompressed = qUncompress(ba);
-        if (!baUncompressed.isEmpty()) {
-            qDebug() << "Data was compressed, uncompressed size:" << baUncompressed.size();
-            ba = baUncompressed;
-        } else {
-            qDebug() << "qUncompress failed or data is not compressed";
-        }
-        
-        return QJsonDocument::fromJson(ba).object();
-    }
 
     QJsonObject sortContainers(const QJsonObject &config) {
         QJsonObject sorted = config;
@@ -102,8 +73,6 @@ private slots:
         QSignalSpy importFinishedSpy(m_coreController->m_importCoreController, &ImportController::importFinished);
         QSignalSpy defaultServerChangedSpy(m_coreController->m_serversRepository, &SecureServersRepository::defaultServerChanged);
         
-        qDebug() << "IMPORTED KEY:" << vpnKey;
-        
         auto importResult = m_coreController->m_importCoreController->extractConfigFromData(vpnKey);
         
         QVERIFY2(importResult.errorCode == ErrorCode::NoError, "Import should succeed");
@@ -123,10 +92,6 @@ private slots:
         QVERIFY2(exportResult.errorCode == ErrorCode::NoError, "Export should succeed");
         QVERIFY2(!exportResult.config.isEmpty(), "Exported config should not be empty");
 
-        qDebug() << "EXPORTED KEY:" << exportResult.config;
-
-        QJsonObject exportedConfig = decodeVpnKey(exportResult.config);
-        
         auto importResult2 = m_coreController->m_importCoreController->extractConfigFromData(exportResult.config);
         QVERIFY2(importResult2.errorCode == ErrorCode::NoError, "Re-import should succeed");
         
@@ -135,9 +100,6 @@ private slots:
         
         QString importedJson = QJsonDocument(sortedImported).toJson(QJsonDocument::Compact);
         QString exportedJson = QJsonDocument(sortedExported).toJson(QJsonDocument::Compact);
-        
-        qDebug() << "IMPORTED JSON:" << importedJson;
-        qDebug() << "EXPORTED JSON:" << exportedJson;
         
         QCOMPARE(exportedJson, importedJson);
     }
