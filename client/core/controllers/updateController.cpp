ce167349deef9c7ec188a8a3e2e2b27e9849fa4b
amneziabot <amneziabot@users.noreply.github.com>
2026-05-25 03:36:35 +0000

    security: omit updater installer response bodies from logs

--- client/core/controllers/updateController.cpp
+++ client/core/controllers/updateController.cpp
@@ -12,7 +12,6 @@
 #include "logger.h"
 #include "version.h"
 #include "core/controllers/gatewayController.h"
-#include "core/utils/api/apiUtils.h"
 #include "core/utils/constants/apiKeys.h"
 #include "core/utils/errorStrings.h"
 #include "core/utils/selfhosted/scriptsRegistry.h"
@@ -264,7 +263,7 @@ void UpdateController::runInstaller()
                 logger.error() << errorString(ErrorCode::ApiConfigTimeoutError);
             } else {
                 QString err = reply->errorString();
-                logger.error() << apiUtils::responseBodyForLog(reply->readAll());
+                logger.error() << "Updater installer download response body omitted from logs";
                 logger.error() << "Network error code:" << QString::number(static_cast<int>(reply->error()));
                 logger.error() << "Error message:" << err;
                 logger.error() << "HTTP status:" << reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
@@ -388,4 +387,3 @@ int UpdateController::runLinuxInstaller(const QString &installerPath)
     return 0;
 }
 #endif
-
