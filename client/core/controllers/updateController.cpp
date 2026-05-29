fdfec6253227e930882fbb82a190762fc9a766ab
amneziabot <amneziabot@users.noreply.github.com>
2026-05-29 21:28:52 +0000

    security: disable unsigned updater installer execution

--- client/core/controllers/updateController.cpp
+++ client/core/controllers/updateController.cpp
@@ -12,10 +12,8 @@
 #include "logger.h"
 #include "version.h"
 #include "core/controllers/gatewayController.h"
-#include "core/utils/api/apiUtils.h"
 #include "core/utils/constants/apiKeys.h"
 #include "core/utils/errorStrings.h"
-#include "core/utils/selfhosted/scriptsRegistry.h"
 
 namespace
 {
@@ -23,13 +21,10 @@ namespace
 
 #if defined(Q_OS_WINDOWS)
     const QLatin1String kInstallerRemoteFileNamePattern("AmneziaVPN_%1_x64.exe");
-    const QString kInstallerLocalPath = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "/AmneziaVPN_installer.exe";
 #elif defined(Q_OS_MACOS)
     const QLatin1String kInstallerRemoteFileNamePattern("AmneziaVPN_%1_macos.pkg");
-    const QString kInstallerLocalPath = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "/AmneziaVPN.pkg";
 #elif defined(Q_OS_LINUX) && !defined(Q_OS_ANDROID)
     const QLatin1String kInstallerRemoteFileNamePattern("AmneziaVPN_%1_linux_x64.tar");
-    const QString kInstallerLocalPath = QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "/AmneziaVPN.tar";
 #endif
 }
 
@@ -222,170 +217,6 @@ QString UpdateController::composeDownloadUrl() const
 void UpdateController::runInstaller()
 {
 #if !defined(Q_OS_ANDROID) && !defined(Q_OS_IOS)
-    if (m_downloadUrl.isEmpty()) {
-        logger.error() << "Download URL is empty";
-        return;
-    }
-
-    QNetworkRequest request;
-    request.setTransferTimeout(30000);
-    request.setUrl(m_downloadUrl);
-
-    QNetworkReply *reply = amnApp->networkManager()->get(request);
-
-    QObject::connect(reply, &QNetworkReply::finished, [this, reply]() {
-        if (reply->error() == QNetworkReply::NoError) {
-            QFile file(kInstallerLocalPath);
-            if (!file.open(QIODevice::WriteOnly)) {
-                logger.error() << "Failed to open installer file for writing:" << kInstallerLocalPath << "Error:" << file.errorString();
-                reply->deleteLater();
-                return;
-            }
-
-            if (file.write(reply->readAll()) == -1) {
-                logger.error() << "Failed to write installer data to file:" << kInstallerLocalPath << "Error:" << file.errorString();
-                file.close();
-                reply->deleteLater();
-                return;
-            }
-
-            file.close();
-
-    #if defined(Q_OS_WINDOWS)
-            runWindowsInstaller(kInstallerLocalPath);
-    #elif defined(Q_OS_MACOS)
-            runMacInstaller(kInstallerLocalPath);
-    #elif defined(Q_OS_LINUX) && !defined(Q_OS_ANDROID)
-            runLinuxInstaller(kInstallerLocalPath);
-    #endif
-        } else {
-            if (reply->error() == QNetworkReply::NetworkError::OperationCanceledError
-                || reply->error() == QNetworkReply::NetworkError::TimeoutError) {
-                logger.error() << errorString(ErrorCode::ApiConfigTimeoutError);
-            } else {
-                QString err = reply->errorString();
-                logger.error() << apiUtils::responseBodyForLog(reply->readAll());
-                logger.error() << "Network error code:" << QString::number(static_cast<int>(reply->error()));
-                logger.error() << "Error message:" << err;
-                logger.error() << "HTTP status:" << reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
-                logger.error() << errorString(ErrorCode::ApiConfigDownloadError);
-            }
-        }
-        reply->deleteLater();
-    });
+    logger.error() << "Automatic updater installer execution is disabled until signed update manifest verification is implemented";
 #endif
 }
-
-#if defined(Q_OS_WINDOWS)
-int UpdateController::runWindowsInstaller(const QString &installerPath)
-{
-    qint64 pid;
-    bool success = QProcess::startDetached(installerPath, QStringList(), QString(), &pid);
-
-    if (success) {
-        logger.info() << "Installation process started with PID:" << pid;
-    } else {
-        logger.error() << "Failed to start installation process";
-        return -1;
-    }
-
-    return 0;
-}
-#endif
-
-#if defined(Q_OS_MACOS)
-int UpdateController::runMacInstaller(const QString &installerPath)
-{
-    // Create temporary directory for extraction
-    QTemporaryDir extractDir;
-    extractDir.setAutoRemove(false);
-    if (!extractDir.isValid()) {
-        logger.error() << "Failed to create temporary directory";
-        return -1;
-    }
-    logger.info() << "Temporary directory created:" << extractDir.path();
-
-    // Create script file in the temporary directory
-    QString scriptPath = extractDir.path() + "/mac_installer.sh";
-    QFile scriptFile(scriptPath);
-    if (!scriptFile.open(QIODevice::WriteOnly)) {
-        logger.error() << "Failed to create script file";
-        return -1;
-    }
-
-    // Get script content from registry
-    QString scriptContent = amnezia::scriptData(amnezia::ClientScriptType::mac_installer);
-    if (scriptContent.isEmpty()) {
-        logger.error() << "macOS installer script content is empty";
-        scriptFile.close();
-        return -1;
-    }
-
-    scriptFile.write(scriptContent.toUtf8());
-    scriptFile.close();
-    logger.info() << "Script file created:" << scriptPath;
-
-    // Make script executable
-    QFile::setPermissions(scriptPath, QFile::permissions(scriptPath) | QFile::ExeUser);
-
-    // Start detached process
-    qint64 pid;
-    bool success =
-            QProcess::startDetached("/bin/bash", QStringList() << scriptPath << extractDir.path() << installerPath, extractDir.path(), &pid);
-
-    if (success) {
-        logger.info() << "Installation process started with PID:" << pid;
-    } else {
-        logger.error() << "Failed to start installation process";
-        return -1;
-    }
-
-    return 0;
-}
-#endif
-
-#if defined(Q_OS_LINUX) && !defined(Q_OS_ANDROID)
-int UpdateController::runLinuxInstaller(const QString &installerPath)
-{
-    // Create temporary directory for extraction
-    QTemporaryDir extractDir;
-    extractDir.setAutoRemove(false);
-    if (!extractDir.isValid()) {
-        logger.error() << "Failed to create temporary directory";
-        return -1;
-    }
-    logger.info() << "Temporary directory created:" << extractDir.path();
-
-    // Create script file in the temporary directory
-    QString scriptPath = extractDir.path() + "/installer.sh";
-    QFile scriptFile(scriptPath);
-    if (!scriptFile.open(QIODevice::WriteOnly)) {
-        logger.error() << "Failed to create script file";
-        return -1;
-    }
-
-    // Get script content from registry
-    QString scriptContent = amnezia::scriptData(amnezia::ClientScriptType::linux_installer);
-    scriptFile.write(scriptContent.toUtf8());
-    scriptFile.close();
-    logger.info() << "Script file created:" << scriptPath;
-
-    // Make script executable
-    QFile::setPermissions(scriptPath, QFile::permissions(scriptPath) | QFile::ExeUser);
-
-    // Start detached process
-    qint64 pid;
-    bool success =
-            QProcess::startDetached("/bin/bash", QStringList() << scriptPath << extractDir.path() << installerPath, extractDir.path(), &pid);
-
-    if (success) {
-        logger.info() << "Installation process started with PID:" << pid;
-    } else {
-        logger.error() << "Failed to start installation process";
-        return -1;
-    }
-
-    return 0;
-}
-#endif
-
